561808ef5c57f1b13e5ff94607eb62785adb8e26
Yasith-Lokuge <yasith@wso2telco.com>
2016-04-28 17:13:03 +0530

    changed to server

--- src/com/coap/server/main/Server.java
+++ src/com/coap/server/main/Server.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package com.coap.client.main;
+package com.coap.server.main;
 
 import java.net.SocketException;
 
